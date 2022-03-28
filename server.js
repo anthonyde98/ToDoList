e7b3213b89b11e4de6561f8877baa6167d673596
anthonyde98 <anthonyde98@gmail.com>
2022-03-27 22:20:42 -0400

    Ajuste para deploy.

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/to-do-list'));
+app.use(express.static(__dirname + '/dist/ToDoList'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/to-do-list/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ToDoList/index.html'));
 });
 
 // default Heroku port
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 5000);
